560ec6b647beb30c56c0fd7901870af5c486300e
Rafael Mendes <faelmob@gmail.com>
2023-02-25 10:37:46 -0300

    first commit

---
+++ src/server.js
@@ -0,0 +1,30 @@
+let emoji = require("node-emoji");
+const fire = emoji.get("fire");
+
+const express = require("express");
+
+const app = express();
+
+//VALORES SÃO OBRIGATÓRIOS
+app.get("/:id/:user", (request, response) => {
+  const { id, user } = request.params;
+
+  response.send(`
+    ID da mensagem: ${id}
+    Usuário: ${user}
+  `);
+});
+
+app.get("/users", (request, response) => {
+  const { page, limit } = request.query;
+
+  response.send(`
+    Página: ${page}.
+    Mostrar: ${limit}
+  `);
+});
+
+const PORT = 3333;
+app.listen(PORT, () =>
+  console.log(`Server is running on Port http://localhost:${PORT} ${fire} `)
+);
