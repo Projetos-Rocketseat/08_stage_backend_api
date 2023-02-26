792da2d833734aa57e85783aeb4dc490fa9675d5
Rafael Mendes <faelmob@gmail.com>
2023-02-25 21:27:42 -0300

    Alterações

--- src/server.js
+++ src/server.js
@@ -1,30 +1,41 @@
-let emoji = require("node-emoji");
+const emoji = require("node-emoji");
 const fire = emoji.get("fire");
 
+require("express-async-errors");
+
+const database = require("./database/sqlite");
+// import database from ("./database/sqlite")
+
+const AppError = require("./utils/AppError");
 const express = require("express");
 
+const routes = require("./routes");
+const { request } = require("express");
+
 const app = express();
+app.use(express.json());
 
-//VALORES SÃO OBRIGATÓRIOS
-app.get("/:id/:user", (request, response) => {
-  const { id, user } = request.params;
+app.use(routes);
 
-  response.send(`
-    ID da mensagem: ${id}
-    Usuário: ${user}
-  `);
-});
+database();
 
-app.get("/users", (request, response) => {
-  const { page, limit } = request.query;
+app.use((error, request, response, next) => {
+  if (error instanceof AppError) {
+    return response.status(error.statusCode).json({
+      status: "error",
+      message: error.message,
+    });
+  }
 
-  response.send(`
-    Página: ${page}.
-    Mostrar: ${limit}
-  `);
+  console.error(error);
+
+  return response.status(500).json({
+    status: "error",
+    message: "Internal server error.",
+  });
 });
 
 const PORT = 3333;
 app.listen(PORT, () =>
-  console.log(`Server running on Port http://localhost:${PORT} ${fire} `)
+  console.log(`Server is running on Port http://localhost:${PORT} ${fire}`)
 );
