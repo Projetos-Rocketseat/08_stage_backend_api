f90163bd1212bc49143c5f5b546189e8ccb840bb
Rafael Mendes <faelmob@gmail.com>
2023-02-28 23:15:58 -0300

    Alteração de commit final

--- src/server.js
+++ src/server.js
@@ -35,5 +35,5 @@ app.use((error, request, response, next) => {
 
 const PORT = 3333;
 app.listen(PORT, () =>
-  console.log(`Server is running on Port http://localhost:${PORT} ${fire}`)
+  console.log(`Server is running on Port http://localhost:${PORT} ${fire}.`)
 );
