b7c136394912c21e599c299a33c7b8e5a31d9290
Rafael Mendes <faelmob@gmail.com>
2023-03-02 22:25:37 -0300

    alteração api

--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -1,12 +1,13 @@
 const { Router } = require("express");
 
 const UsersController = require("../controllers/UsersController");
+const ensureAuthenticated = require("../middlewares/ensureAuthenticated");
 
 const usersRouters = Router();
 
 const usersController = new UsersController();
 
 usersRouters.post("/", usersController.create);
-usersRouters.put("/:id", usersController.update);
+usersRouters.put("/:id", ensureAuthenticated, usersController.update);
 
 module.exports = usersRouters;
